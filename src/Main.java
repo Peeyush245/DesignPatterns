436d1a25364cd5dfc4eff56c1849e714ab6f57d7
peeyush245 <peeyushpandey245@gmail.com>
2025-11-23 22:51:47 +0530

    Builder Pattern

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        User user = User.builder()
+                .setAge(29)
+                .setName("Peeyush")
+                .setEmail("peeyushpandey245@gmail.com")
+                .build();
+
+        System.out.println(user.toString());
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,76 @@
+public class User {
+    private String name;
+    private int age;
+    private String email;
+
+    private User(Builder builder) {
+        this.name = builder.getName();
+        this.age = builder.getAge();
+        this.email = builder.getEmail();
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String name;
+        private int age;
+        private String email;
+
+        public String getName() {
+            return name;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public Builder setAge(int age) {
+            if (age < 0) {
+                throw new IllegalArgumentException("Age cannot be negative");
+            }
+            this.age = age;
+            return this;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
+}
